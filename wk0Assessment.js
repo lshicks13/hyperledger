097f2950f683db8d857036cbc49bf1c0edab0340
LSH <lsh13@localhost.localdomain>
2019-04-09 22:34:14 -0400

    Task 4 Complete

--- wk0Assessment.js
+++ wk0Assessment.js
@@ -71,20 +71,20 @@ var talkingCalendar = function(date) {
     if (dt.getDate() % 10 == 1 && dt.getDate() != 11) {
         d += 'st';
         if(dt.getDate() < 4){
-            d = d[1] + 'st'
+            d = d[1] + 'st';
         }
     } else if(dt.getDate() % 10 == 2 && dt.getDate() != 12) {
-        d += 'nd'
+        d += 'nd';
         if(dt.getDate() < 4){
-            d = d[1] + 'nd'
+            d = d[1] + 'nd';
         }
     } else if(dt.getDate() % 10 == 3 && dt.getDate() != 13) {
-        d += 'rd'
+        d += 'rd';
         if(dt.getDate() < 4){
-            d = d[1] + 'rd'
+            d = d[1] + 'rd';
         }
     } else {
-        d += 'th'
+        d += 'th';
     }
     return mth + " " + d + ", " + y;
 };
@@ -92,3 +92,49 @@ var talkingCalendar = function(date) {
 console.log(talkingCalendar("2017/12/02"));
 console.log(talkingCalendar("2007/11/11"));
 console.log(talkingCalendar("1987/08/24"));
+
+//Task 4: change Calculator
+var calculateChange = function(total, cash) {
+    // My code
+    var chg = cash - total;
+    var change = {};
+    while (chg > 0){
+        if(chg >= 1000) {
+            change["ten"] = Math.trunc(chg/1000);
+            chg -= (1000 * change.ten);
+        }
+        if(chg >= 500){
+            change["fiveDollar"] = Math.trunc(chg/500);
+            chg -= (500 * change.fiveDollar);
+        }
+        if(chg >= 200){
+            change["twoDollar"] = Math.trunc(chg/200);
+            chg -= (200 * change.twoDollar);
+        }
+        if(chg >= 100){
+            change["dollar"] = Math.trunc(chg/100);
+            chg -= (100 * change.dollar);
+        }
+        if(chg >= 25){
+            change["quarter"] = Math.trunc(chg/25);
+            chg -= (25 * change.quarter);
+        }
+        if(chg >= 10){
+            change["dime"] = Math.trunc(chg/10);
+            chg -= (10 * change.dime);
+        }
+        if(chg >= 5){
+            change["nickel"] = Math.trunc(chg/5);
+            chg -= (5 * change.nickel);
+        }
+        if(chg >= 1){
+            change["penny"] = Math.trunc(chg/1);
+            chg -= (1 * change.penny); 
+        }
+    }
+    return change;
+  };
+  
+  console.log(calculateChange(1787, 2000));
+  console.log(calculateChange(2623, 4000));
+  console.log(calculateChange(501, 1000));
